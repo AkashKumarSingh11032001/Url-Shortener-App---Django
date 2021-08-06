7ebd6fced146499df6c76616e84e403275469c89
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-08-06 22:00:14 +0530

    url

--- Url_shortener_Web_App/shortner/urls.py
+++ Url_shortener_Web_App/shortner/urls.py
@@ -1,9 +1,8 @@
-from django.contrib import admin
 from django.urls import path
-from shortner import views
+from . import views
 
 urlpatterns = [
-    path('',views.index,name='index'),
-    path('create',views.create,name='create'),
-    path('/<str:pk>',views.go,name='go'),
+    path('', views.index, name='index'),
+    path('create', views.create, name='create'),
+    path('<str:pk>', views.go, name='go')
 ]
