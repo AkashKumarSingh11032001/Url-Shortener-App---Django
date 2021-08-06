0af1d8b9684605b30c4179c34dea71c1c292dcd2
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-08-06 21:40:23 +0530

    url

--- Url_shortener_Web_App/shortner/views.py
+++ Url_shortener_Web_App/shortner/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+import uuid
+from .models import Url
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def create(request):
-    pass
+    if request.method == 'POST':
+        
+        link = request.POST['link']
+        uid = str(uuid.uuid4())[:5]
+
+        # savinf data in database
+        new_url = Url(link=link,uuid=uid)
+        new_url.save()
+
+        return HttpResponse(uid)
